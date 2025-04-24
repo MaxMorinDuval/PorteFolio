3e4db84672434a86c585e6ccbde6c897d9a4a692
MaxMorinDuval <164524599+MaxMorinDuval@users.noreply.github.com>
2025-04-24 12:16:00 +0200

    Add files via upload

---
+++ asset/template/asset/javascript/app.js
@@ -0,0 +1,33 @@
+var nextBtn = document.querySelector('.next'),
+    prevBtn = document.querySelector('.prev'),
+    carousel = document.querySelector('.carousel'),
+    list = document.querySelector('.list'), 
+    item = document.querySelectorAll('.item'),
+    runningTime = document.querySelector('.carousel .timeRunning') 
+
+
+
+nextBtn.onclick = function(){
+    showSlider('next')
+}
+
+prevBtn.onclick = function(){
+    showSlider('prev')
+}
+
+function showSlider(type) {
+    let sliderItemsDom = list.querySelectorAll('.carousel .list .item')
+    if(type === 'next'){
+        list.appendChild(sliderItemsDom[0])
+        carousel.classList.add('next')
+    } else{
+        list.prepend(sliderItemsDom[sliderItemsDom.length - 1])
+        carousel.classList.add('prev')
+    }
+
+}
+
+
+document.querySelector('.back-button').addEventListener('click', () => {
+    window.history.back();
+});
